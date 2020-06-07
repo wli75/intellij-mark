1f748321c4846a7588961161e056fc395080c09b
wli75 <clara.li.wing.yee@gmail.com>
2020-06-07 15:01:52 +0800

    Remove debug lines and cleanup

--- intellij-mark.el
+++ intellij-mark.el
@@ -10,15 +10,20 @@
 (defvar intellij-mark-ring)
 (defvar intellij-mark-ring-pos)
 
-(defun cursor ()
-  (vector (buffer-file-name) (point)))
+(defun set-cursor (file point)
+  "Reuse/create buffer that points to file, and set cursor position to point."
+  (let ((buffer (find-buffer-visiting file)))
+    (when (equal buffer nil)
+      (setq buffer (find-file-noselect file)))
+    (let ((window (get-buffer-window buffer)))
+      (when (not (equal window nil))
+	(select-window window))
+      (switch-to-buffer buffer)))
+  (goto-char point))
 
-(defun remove-mark-ring (index)
-  "Remove elements with position <= index in the mark ring."
-  (let ((pos 0))
-    (while (<= pos index)
-      (ring-remove intellij-mark-ring 0)
-      (setq pos (+ pos 1)))))
+(defun cursor ()
+  "Return current buffer file name and point position."
+  (vector (buffer-file-name) (point)))
 
 (defun curr-buffer-is-visiting-file ()
   "Returns t if the current buffer is visiting a file, nil otherwise."
@@ -33,19 +38,29 @@
 
 (defun should-push-mark-ring (index curr-cursor)
   "Compare cursor with the element in the mark ring at index, and return t if cursor should be pushed to the mark ring, nil otherwise."
-  (message "in should-push-mark-ring")
-  (message "curr-buffer-is-visiting-file:")
-  (prin1 (curr-buffer-is-visiting-file))
-  (message "index:")
-  (prin1 index)
   (and (curr-buffer-is-visiting-file)
        (or (< index 0)
 	   (let ((prev-cursor (ring-ref intellij-mark-ring index)))
              (not (prev-curr-cursors-are-the-same prev-cursor curr-cursor))))))
 
+(defun push-mark-ring ()
+  "Insert cursor to the mark ring at (intellij-mark-ring-pos + 1), delete elements after the inserted position, and update intellij-mark-ring-pos."
+  (let ((cur (cursor)))
+    (when (should-push-mark-ring intellij-mark-ring-pos cur)
+      (remove-mark-ring (- intellij-mark-ring-pos 1))
+      (ring-insert intellij-mark-ring cur)
+      (setq intellij-mark-ring-pos 0))))
+
+(defun remove-mark-ring (index)
+  "Remove elements with position <= index in the mark ring."
+  (let ((pos 0))
+    (while (<= pos index)
+      (ring-remove intellij-mark-ring 0)
+      (setq pos (+ pos 1)))))
+
 (defun print-intellij-mark-ring ()
+  "Print contents in intellij mark ring."
   (interactive)
-  (message "intellij mark ring")
   (let ((pos 0))
     (while (< pos (ring-length intellij-mark-ring))
       (message "pos: ")
@@ -53,44 +68,10 @@
       (message "elem: ")
       (prin1 (ring-ref intellij-mark-ring pos))
       (setq pos (+ pos 1)))))
-
-(defun push-mark-ring ()
-  "Insert cursor to the mark ring at (intellij-mark-ring-pos + 1), delete elements after the inserted position, and update intellij-mark-ring-pos."
-  (message "in push-mark-ring")
-  (message "intellij-mark-ring-pos:")
-  (prin1 intellij-mark-ring-pos)
-  (let ((cur (cursor)))
-    (when (should-push-mark-ring intellij-mark-ring-pos cur)
-      (remove-mark-ring (- intellij-mark-ring-pos 1))
-      (ring-insert intellij-mark-ring cur)
-      (setq intellij-mark-ring-pos 0)
-;;      (message "intellij-mark-ring:")
-;;      (print-mark-ring)
-;;      (message "intellij-mark-ring-pos:")
-;;      (prin1 intellij-mark-ring-pos))))
-)))
       
 (defun intellij-mark-post-command-handler ()
-;;  (message "in intellij-mark-post-command-handler")
-;;  (message "this-command:")
-;;  (prin1 this-command)
   (when (member this-command intellij-mark-ring-cmds)
     (push-mark-ring)))
-
-(defun set-cursor (file point)
-;;  (message "in set-cursor")
-  (let ((buffer (find-buffer-visiting file)))
-    (when (equal buffer nil)
-      (setq buffer (find-file-noselect file)))
-;;    (message "buffer:")
-;;    (prin1 buffer)
-    (let ((window (get-buffer-window buffer)))
-      (when (not (equal window nil))
-;;	(message "window:")
-;;	(prin1 window)
-	(select-window window))
-      (switch-to-buffer buffer)))
-  (goto-char point))
 
 (defun intellij-mark-ring-prev ()
   "Go to the previous cursor position."
@@ -100,24 +81,17 @@
     (let ((file-point (ring-ref intellij-mark-ring (+ intellij-mark-ring-pos 1))))
       (let ((file (aref file-point 0)) (point (aref file-point 1)))
 	(set-cursor file point)
-	(setq intellij-mark-ring-pos (+ intellij-mark-ring-pos 1))
-;;	(message "intellij-mark-ring-pos:")
-;;	(prin1 intellij-mark-ring-pos)))))
-))))
+	(setq intellij-mark-ring-pos (+ intellij-mark-ring-pos 1))))))
 	
 (defun intellij-mark-ring-next ()
   "Go to the next cursor position."
   (interactive)
-;;  (message "in intellij-mark-ring-next")
   (when (and intellij-mark-mode
 	     (> intellij-mark-ring-pos 0))
     (let ((file-point (ring-ref intellij-mark-ring (- intellij-mark-ring-pos 1))))
       (let ((file (aref file-point 0)) (point (aref file-point 1)))
 	(set-cursor file point)
-	(setq intellij-mark-ring-pos (- intellij-mark-ring-pos 1))
-;;	(message "intellij-mark-ring-pos:")
-;;	(prin1 intellij-mark-ring-pos)))))
-))))
+	(setq intellij-mark-ring-pos (- intellij-mark-ring-pos 1))))))
 	
 (define-minor-mode intellij-mark-mode
   "Intellij mark mode."
@@ -125,20 +99,10 @@
   :global t
   (if intellij-mark-mode
       (progn
-	(message "in intellij-mark-mode")
         (setq intellij-mark-ring (make-ring intellij-mark-ring-length))
         (setq intellij-mark-ring-pos -1)
 	(add-hook 'post-command-hook 'intellij-mark-post-command-handler))
     (remove-hook 'post-command-hook 'intellij-mark-post-command-handler)))
-
-;; (defun intellij-mark-mode-maybe ()
-;;   (if (not (minibufferp (current-buffer)))
-;;       (intellij-mark-mode 1)))
-
-;; (define-global-minor-mode global-intellij-mark-mode
-;;   "Intellij mark mode."
-;;   intellij-mark-mode intellij-mark-mode-maybe
-;;   :group 'intellij-mark)
 
 (global-set-key (kbd "M-b") 'intellij-mark-ring-prev)
 (global-set-key (kbd "M-f") 'intellij-mark-ring-next)
